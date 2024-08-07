d11192727f3df87d611a9f3511500f642707dd8e
Godwin <godwinchuks032@gmail.com>
2024-08-08 00:00:36 +0100

    get all the files

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -72,6 +72,59 @@ class FilesController {
       queue.add({ userId, fileId: newFile.insertedId });
     }
   }
+
+  // New method to retrieve file by ID
+  static async getShow(req, res) {
+    const token = req.header('X-Token');
+    const id = await redisClient.get(`auth_${token}`);
+    if (!id) return res.status(401).json({ error: 'Unauthorized' });
+
+    const user = await dbClient.db.collection('users').findOne({ _id: ObjectId(id) });
+    if (!user) return res.status(401).json({ error: 'Unauthorized' });
+
+    const fileId = req.params.id;
+    const file = await dbClient.db.collection('files').findOne({
+      _id: ObjectId(fileId),
+      userId: user._id,
+    }, {
+      projection: {
+        id: '$_id', _id: 0, name: 1, type: 1, isPublic: 1, parentId: 1, userId: 1,
+      },
+    });
+
+    if (file) return res.status(200).json(file);
+    return res.status(404).json({ error: 'Not found' });
+  }
+
+  // New method to retrieve files with pagination
+  static async getIndex(req, res) {
+    const token = req.header('X-Token');
+    const id = await redisClient.get(`auth_${token}`);
+    if (!id) return res.status(401).json({ error: 'Unauthorized' });
+
+    const user = await dbClient.db.collection('users').findOne({ _id: ObjectId(id) });
+    if (!user) return res.status(401).json({ error: 'Unauthorized' });
+
+    const { parentId = '0' } = req.query;
+    const page = parseInt(req.query.page) || 0;
+
+    const filter = { userId: user._id };
+    if (parentId !== '0') filter.parentId = ObjectId(parentId);
+
+    const fileCollection = dbClient.db.collection('files');
+    const files = await fileCollection.aggregate([
+      { $match: filter },
+      { $skip: page * 20 },
+      { $limit: 20 },
+      {
+        $project: {
+          id: '$_id', _id: 0, userId: 1, name: 1, type: 1, isPublic: 1, parentId: 1,
+        },
+      },
+    ]).toArray();
+
+    res.status(200).json(files);
+  }
 }
 
 export default FilesController;
